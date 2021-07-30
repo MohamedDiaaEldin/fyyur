d241d377c17e3162424fa0ab9f26d29fc5b44fdd
mohamed <mdiaa404@gmail.com>
2021-07-30 14:46:51 +0200

    implementing update venue and updating record data in Venue and Genre Tables

--- app.py
+++ app.py
@@ -270,19 +270,6 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
     form = ArtistForm()
-    # artist = {
-    #     "id": 4,
-    #     "name": "Guns N Petals",
-    #     "genres": ["Rock n Roll"],
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "phone": "326-123-5000",
-    #     "website": "https://www.gunsnpetalsband.com",
-    #     "facebook_link": "https://www.facebook.com/GunsNPetals",
-    #     "seeking_venue": True,
-    #     "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    #     "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-    # }
     artist = Artist.query.get(artist_id)
 
     if artist: 
@@ -296,16 +283,11 @@ def edit_artist(artist_id):
         form.seeking_venue.data = artist.seeking_venue
         form.seeking_description.data = artist.seeking_venue_description        
         form.genres.data = Genre.query.filter_by(artist_id=artist_id)
-
-
-    # TODO: populate form with fields from artist with ID <artist_id>
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
-    # artist record with ID <artist_id> using the new attributes
     try:        
         artist = Artist.query.get(artist_id)
         artist.name = request.form['name']
@@ -324,10 +306,6 @@ def edit_artist_submission(artist_id):
         for genre in request.form.getlist('genres'):
             db.session.add(Genre(name=genre, artist_id=artist_id))            
             db.session.commit()            
-        else:
-            print("singer NOT updated")
-            db.session.rollback()
-        
     except:
         db.session.rollback()
         print('error happend while updating artist')        
@@ -337,28 +315,46 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
+    venue = Venue.query.get(venue_id)
+    if venue :
+        form.name.data = venue.name
+        form.city.data = venue.city
+        form.state.data = venue.state
+        form.phone.data = venue.phone
+        form.facebook_link.data = venue.facebook_link
+        form.image_link.data = venue.image_link
+        form.website_link.data = venue.website_link
+        form.seeking_talent.data = venue.seeking_talent
+        form.seeking_description.data = venue.seeking_description        
+        form.genres.data = Genre.query.filter_by(venue_id=venue_id)
     # TODO: populate form with values from venue with ID <venue_id>
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
-    # venue record with ID <venue_id> using the new attributes
+    try:
+        venue = Venue.query.get(venue_id)
+        venue.name = request.form['name']
+        venue.city = request.form['city']
+        venue.state = request.form['state']
+        venue.address = request.form['address']
+        venue.phone = request.form['phone']
+        venue.facebook_link = request.form['facebook_link']
+        venue.website_link = request.form['website_link']
+        venue.seeking_talent =  True if 'seeking_talent' in request.form else False 
+        venue.seeking_description = request.form['seeking_description']
+        venue.image_link = request.form['image_link']
+        db.session.commit()
+        ## updating genres
+        Genre.query.filter_by(venue_id=venue_id).delete()
+        db.session.commit()
+        for genre in request.form.getlist('genres'):
+            db.session.add(Genre(name=genre, venue_id=venue_id))
+            db.session.commit()
+    except:
+        db.session.rollback()
+        print('error happend while updating venue')
     return redirect(url_for('show_venue', venue_id=venue_id))
 
 
